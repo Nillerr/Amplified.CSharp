884d7120cb46360d2c3bd8b51b586bd2cfa2ec2e
Nicklas Sander Jensen <nsj@hesehus.dk>
2016-12-22 12:22:53 +0100

    Imported JetBrains annotations into project

---
+++ Amplified.CSharp/src/JetBrains/Annotations/CanBeNullAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace JetBrains.Annotations
+{
+    /// <summary>
+    /// Indicates that the value of the marked element could be <c>null</c> sometimes,
+    /// so the check for <c>null</c> is necessary before its usage.
+    /// </summary>
+    /// <example><code>
+    /// [CanBeNull] object Test() =&gt; null;
+    ///
+    /// void UseTest() {
+    ///   var p = Test();
+    ///   var s = p.ToString(); // Warning: Possible 'System.NullReferenceException'
+    /// }
+    /// </code></example>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Parameter | AttributeTargets.Delegate | AttributeTargets.GenericParameter)]
+    [Conditional("JETBRAINS_ANNOTATIONS")]
+    internal sealed class CanBeNullAttribute : Attribute
+    {
+    }
+}
---
+++ Amplified.CSharp/src/JetBrains/Annotations/NotNullAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace JetBrains.Annotations
+{
+    /// <summary>
+    /// Indicates that the value of the marked element could never be <c>null</c>.
+    /// </summary>
+    /// <example><code>
+    /// [NotNull] object Foo() {
+    ///   return null; // Warning: Possible 'null' assignment
+    /// }
+    /// </code></example>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Parameter | AttributeTargets.Delegate | AttributeTargets.GenericParameter)]
+    [Conditional("JETBRAINS_ANNOTATIONS")]
+    internal sealed class NotNullAttribute : Attribute
+    {
+    }
+}
