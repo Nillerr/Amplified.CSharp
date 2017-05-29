5efa20cedf37cd5c026314fb8e0618ddae8f22f9
Nicklas Sander Jensen <nsj@hesehus.dk>
2017-05-29 14:03:28 +0200

    Adds more tests

---
+++ Amplified.CSharp.Tests/None/Match__Action.cs
@@ -0,0 +1,29 @@
+using Xunit;
+using static Amplified.CSharp.Units;
+
+namespace Amplified.CSharp
+{
+    // ReSharper disable once InconsistentNaming
+    public class None__Match__Action
+    {
+        [Fact]
+        public void WithLambda()
+        {
+            var source = new None();
+            var result = source.Match(() => { });
+            Assert.Equal(result, Unit());
+        }
+        
+        [Fact]
+        public void WithMethodReference()
+        {
+            void Match()
+            {
+            }
+
+            var source = new None();
+            var result = source.Match(Match);
+            Assert.Equal(result, Unit());
+        }
+    }
+}
---
+++ Amplified.CSharp.Tests/None/Match__Action_None.cs
@@ -0,0 +1,29 @@
+using Xunit;
+using static Amplified.CSharp.Units;
+
+namespace Amplified.CSharp
+{
+    // ReSharper disable once InconsistentNaming
+    public class None__Match__Action_None
+    {
+        [Fact]
+        public void WithLambda()
+        {
+            var source = new None();
+            var result = source.Match(none => { });
+            Assert.Equal(result, Unit());
+        }
+        
+        [Fact]
+        public void WithMethodReference()
+        {
+            void Match(None none)
+            {
+            }
+
+            var source = new None();
+            var result = source.Match(Match);
+            Assert.Equal(result, Unit());
+        }
+    }
+}
