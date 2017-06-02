6684a237435ff1f85ca02c8a7129886d4bbf9c99
Nicklas Jensen <nicklas@njensen.net>
2017-06-02 10:41:36 +0200

    Adds more unit tests

--- Amplified.CSharp.Tests/Extensions/Enumerable/LastOrNone.cs
+++ Amplified.CSharp.Tests/Extensions/Enumerable/LastOrNone.cs
@@ -10,22 +10,22 @@ namespace Amplified.CSharp
 {
     // ReSharper disable once InconsistentNaming
     [SuppressMessage("ReSharper", "CollectionNeverUpdated.Local")]
-    public class Enumerable__FirstOrNone
+    public class Enumerable__LastOrNone
     {
-        #region Maybe<TSource> FirstOrNone<TSource>()
+        #region Maybe<TSource> LastOrNone<TSource>()
 
         [Fact]
         public void WhenSourceIsNull_ThrowsArgumentNullException()
         {
             IEnumerable<int> source = null;
-            Assert.Throws<ArgumentNullException>(() => source.FirstOrNone());
+            Assert.Throws<ArgumentNullException>(() => source.LastOrNone());
         }
         
         [Fact]
         public void WhenEmpty_ReturnsNone()
         {
             var source = new HashSet<int>().AsEnumerable();
-            var result = source.FirstOrNone();
+            var result = source.LastOrNone();
             result.MustBeNone();
         }
         
@@ -33,25 +33,25 @@ namespace Amplified.CSharp
         public void WhenOneElement_ReturnsSomeOfSingle()
         {
             var source = Enumerable.Range(1, 1);
-            var result = source.FirstOrNone();
+            var result = source.LastOrNone();
             var value = result.MustBeSome();
             Assert.Equal(1, value);
         }
         
         [Fact]
-        public void WhenTwoElements_ReturnsSomeOfFirst()
+        public void WhenTwoElements_ReturnsSomeOfLast()
         {
             var source = Enumerable.Range(1, 2);
-            var result = source.FirstOrNone();
+            var result = source.LastOrNone();
             var value = result.MustBeSome();
-            Assert.Equal(1, value);
+            Assert.Equal(2, value);
         }
         
         [Fact]
         public void WhenEmptyList_ReturnsNone()
         {
             var source = new List<int>().AsEnumerable();
-            var result = source.FirstOrNone();
+            var result = source.LastOrNone();
             result.MustBeNone();
         }
         
@@ -59,36 +59,36 @@ namespace Amplified.CSharp
         public void WhenOneElementList_ReturnsSomeOfSingle()
         {
             var source = new List<int> { 1 }.AsEnumerable();
-            var result = source.FirstOrNone();
+            var result = source.LastOrNone();
             var value = result.MustBeSome();
             Assert.Equal(1, value);
         }
         
         [Fact]
-        public void WhenTwoElementsList_ReturnsSomeOfFirst()
+        public void WhenTwoElementsList_ReturnsSomeOfSingle()
         {
             var source = new List<int> {1, 2}.AsEnumerable();
-            var result = source.FirstOrNone();
+            var result = source.LastOrNone();
             var value = result.MustBeSome();
-            Assert.Equal(1, value);
+            Assert.Equal(2, value);
         }
         
         #endregion
         
-        #region Maybe<TSource> FirstOrNone<TSource>(Func<TSource, bool> predicate)
+        #region Maybe<TSource> LastOrNone<TSource>(Func<TSource, bool> predicate)
 
         [Fact]
         public void WithPredicate_WhenSourceIsNull_ThrowsArgumentNullException()
         {
             IEnumerable<int> source = null;
-            Assert.Throws<ArgumentNullException>(() => source.FirstOrNone(it => true));
+            Assert.Throws<ArgumentNullException>(() => source.LastOrNone(it => true));
         }
 
         [Fact]
         public void WithPredicate_WhenPredicateIsNull_ThrowsArgumentNullException()
         {
             var source = Enumerable.Empty<int>();
-            Assert.Throws<ArgumentNullException>(() => source.FirstOrNone(null));
+            Assert.Throws<ArgumentNullException>(() => source.LastOrNone(null));
         }
         
         #region Empty
@@ -97,7 +97,7 @@ namespace Amplified.CSharp
         public void WithTruePredicate_WhenEmpty_ReturnsNone()
         {
             var source = new HashSet<int>().AsEnumerable();
-            var result = source.FirstOrNone(it => true);
+            var result = source.LastOrNone(it => true);
             result.MustBeNone();
         }
         
@@ -105,7 +105,7 @@ namespace Amplified.CSharp
         public void WithFalsePredicate_WhenEmpty_ReturnsNone()
         {
             var source = new HashSet<int>().AsEnumerable();
-            var result = source.FirstOrNone(it => false);
+            var result = source.LastOrNone(it => false);
             result.MustBeNone();
         }
         
@@ -114,7 +114,7 @@ namespace Amplified.CSharp
         {
             var rec = new Recorder();
             var source = new HashSet<int>().AsEnumerable();
-            var result = source.FirstOrNone(rec.Record((int it) => false));
+            var result = source.LastOrNone(rec.Record((int it) => false));
             result.MustBeNone();
             rec.MustHaveExactly(0.Invocations());
         }
@@ -127,7 +127,7 @@ namespace Amplified.CSharp
         public void WithTruePredicate_WhenOneElement_ReturnsSome()
         {
             var source = Enumerable.Range(1, 1);
-            var result = source.FirstOrNone(it => true);
+            var result = source.LastOrNone(it => true);
             var value = result.MustBeSome();
             Assert.Equal(1, value);
         }
@@ -136,7 +136,7 @@ namespace Amplified.CSharp
         public void WithFalsePredicate_WhenOneElement_ReturnsNone()
         {
             var source = Enumerable.Range(1, 1);
-            var result = source.FirstOrNone(it => false);
+            var result = source.LastOrNone(it => false);
             result.MustBeNone();
         }
         
@@ -145,7 +145,7 @@ namespace Amplified.CSharp
         {
             var rec = new Recorder();
             var source = Enumerable.Range(1, 1);
-            var result = source.FirstOrNone(rec.Record((int it) => true));
+            var result = source.LastOrNone(rec.Record((int it) => true));
             var value = result.MustBeSome();
             Assert.Equal(1, value);
             rec.MustHaveExactly(1.Invocations());
@@ -156,19 +156,19 @@ namespace Amplified.CSharp
         #region Two Elements
         
         [Fact]
-        public void WithTruePredicate_WhenTwoElements_ReturnsFirst()
+        public void WithTruePredicate_WhenTwoElements_ReturnsLast()
         {
             var source = Enumerable.Range(1, 2);
-            var result = source.FirstOrNone(it => true);
+            var result = source.LastOrNone(it => true);
             var value = result.MustBeSome();
-            Assert.Equal(1, value);
+            Assert.Equal(2, value);
         }
         
         [Fact]
         public void WithFalsePredicate_WhenTwoElements_ReturnsNone()
         {
             var source = Enumerable.Range(1, 2);
-            var result = source.FirstOrNone(it => false);
+            var result = source.LastOrNone(it => false);
             result.MustBeNone();
         }
         
@@ -180,7 +180,7 @@ namespace Amplified.CSharp
         public void WithPredicateMatchingFirst_WhenThreeElements_ReturnsSome()
         {
             var source = Enumerable.Range(1, 3);
-            var result = source.FirstOrNone(it => it == 1);
+            var result = source.LastOrNone(it => it == 1);
             var value = result.MustBeSome();
             Assert.Equal(1, value);
         }
@@ -189,7 +189,7 @@ namespace Amplified.CSharp
         public void WithPredicateMatchingMiddle_WhenThreeElements_ReturnsSome()
         {
             var source = Enumerable.Range(1, 3);
-            var result = source.FirstOrNone(it => it == 2);
+            var result = source.LastOrNone(it => it == 2);
             var value = result.MustBeSome();
             Assert.Equal(2, value);
         }
@@ -198,29 +198,29 @@ namespace Amplified.CSharp
         public void WithPredicateMatchingLast_WhenThreeElements_ReturnsSomeOfLast()
         {
             var source = Enumerable.Range(1, 3);
-            var result = source.FirstOrNone(it => it == 3);
+            var result = source.LastOrNone(it => it == 3);
             var value = result.MustBeSome();
             Assert.Equal(3, value);
         }
 
         [Fact]
-        public void WithPredicateMatchingLastAndNextToLast_WhenThreeElements_ReturnsSomeOfNextToLast()
+        public void WithPredicateMatchingLastAndNextToLast_WhenThreeElements_ReturnsSomeOfLast()
         {
             var source = new []{1, 2, 3, 3}.AsEnumerable();
-            var result = source.FirstOrNone(it => it == 3);
+            var result = source.LastOrNone(it => it == 3);
             var value = result.MustBeSome();
             Assert.Equal(3, value);
         }
 
         [Fact]
-        public void WithPredicate_WhenThreeElements_InvokesPredicateOneTime()
+        public void WithPredicate_WhenThreeElements_InvokesPredicateThreeTimes()
         {
             var rec = new Recorder();
             var source = Enumerable.Range(1, 3);
-            var result = source.FirstOrNone(rec.Record((int it) => true));
+            var result = source.LastOrNone(rec.Record((int it) => true));
             var value = result.MustBeSome();
-            Assert.Equal(1, value);
-            rec.MustHaveExactly(1.Invocations());
+            Assert.Equal(3, value);
+            rec.MustHaveExactly(3.Invocations());
         }
         
         #endregion
